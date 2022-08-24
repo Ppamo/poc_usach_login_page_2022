5b53232bdb33fc8f3cc309cba686d2ca142a51a3
Ppamo <pablo@ppamo.cl>
2022-08-24 15:26:02 -0400

    Update password mask logic

--- html/js/logic.js
+++ html/js/logic.js
@@ -1,13 +1,25 @@
+const dummy = document.getElementById('k_dummy');
+const k_input = document.getElementById('k_input');
+
 function button_on_click(){
-	alert(1);
+	console.log(k_input.value);
+}
+
+function update_value(e){
+	const text = Array(e.target.value.length).fill('*').join('');
+	dummy.innerHTML = text;
+
 }
 
 function page_onload(){
-	const button=document.getElementById("submit_button");
-	button.addEventListener("click", button_on_click, false);
 	const queryString = window.location.search;
 	const parameters = new URLSearchParams(queryString);
 	const value = parameters.get('theme');
+	const button=document.getElementById("submit_button");
+
+	button.addEventListener("click", button_on_click, false);
+	k_input.addEventListener('input', update_value);
+
 	if (value == "default") {
 		document.getElementById("cover").style.visibility = 'hidden';
 	}
